e461371053ba9946b4da480531bea56338670fff
Kay Ousterhout <kayousterhout@gmail.com>
2014-04-17 14:53:04 -0700

    Added (hacky!) script to run all queries.

---
+++ runner/run_all.sh
@@ -0,0 +1,41 @@
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=1a --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=1b --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=1c --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=2a --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=2b --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=2c --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=3a --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=3b --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=3c --num-trials=1 
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=4 --num-trials=1 
+
+echo "Running no-cache queries..."
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=1a --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=1b --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=1c --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=2a --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=2b --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=2c --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=3a --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=3b --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=3c --num-trials=1 --shark-no-cache
+
+./run-query.sh --shark --shark-host=ec2-54-211-67-60.compute-1.amazonaws.com --shark-identity-file=../../spark-1/ec2/newkey.pem --query-num=4 --num-trials=1 --shark-no-cache
