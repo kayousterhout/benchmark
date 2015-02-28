334cdddee05406f575ec8a494b69acebf96b0009
Kay Ousterhout <kayousterhout@gmail.com>
2015-02-28 12:19:07 -0800

    Fixed url count to avoid malformed urls

--- runner/udf/url_count.py
+++ runner/udf/url_count.py
@@ -20,15 +20,19 @@ cur_page = "NONE"
 # For each page, we buffer URL counts in memory
 url_count = {}
 
+def print_url_count():
+  if '\t' not in cur_page:
+    for (url,count) in url_count.items():
+      if '\t' not in url:
+        print "%s\t%s\t%s" % (cur_page, url, count)
+
 for line in sys.stdin:
   if (line[0:4] == "http" and len(line.split(" ")) == 5):
     cur_page = line.split(" ")[0]
-    for (url, count) in url_count.items():
-      print "%s\t%s\t%s" % (cur_page, url, count)
+    print_url_count()
     url_count = {}
 
   for url in url_regex.findall(line):
      url_count[url] = url_count.get(url, 0) + 1
 
-for (url, count) in url_count.items():
-  print "%s\t%s\t%s" % (cur_page, url, count)
+print_url_count()
